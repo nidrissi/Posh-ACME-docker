0c7d99f040048f653887d1c3f4ebda8476908a52
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-04-28 16:51:45 +0200

    manual deploy steps

---
+++ deploy.ps1
@@ -0,0 +1,30 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $ResourceGroup,
+    [Parameter()]
+    [string]
+    $ZoneResourceGroup,
+    [Parameter()]
+    [string]
+    $ZoneName
+)
+
+# Deploy
+$Deployment = New-AzResourceGroupDeployment -Verbose -Name 'Posh-ACME' -ResourceGroupName $ResourceGroup -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json
+# Immediately stop the container
+Invoke-AzResourceAction -ResourceId $ContainerGroup.Id -Action stop -Force
+
+# Get the Container Group
+$ContainerGroup = Get-AzContainerGroup -ResourceGroupName rg-Test -Name $Deployment.Outputs.container.Value
+
+# The zone
+$ZoneResource = Get-AzResource -ResourceGroupName $ZoneResourceGroup -Name $ZoneName
+$ZoneScope = $ZoneResource.ResourceId
+
+# Roles
+New-AzRoleAssignment -ObjectId $ContainerGroup.Identity.PrincipalId -RoleDefinitionName "DNS Zone Contributor" -Scope $ZoneScope
+
+# Start the container, for real this time
+Invoke-AzResourceAction -ResourceId $ContainerGroup.Id -Action start -Force
